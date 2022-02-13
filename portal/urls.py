1ca463c1275c6678dd249a94f6adf3ca44306157
lisanka93 <lisa.chalaguine@gmail.com>
2022-02-13 16:46:07 +0000

    deleted unnecessary apostrophe from delete url

--- portal/urls.py
+++ portal/urls.py
@@ -8,7 +8,7 @@ urlpatterns= [
     path("add/", PostAdd.as_view(), name="add"),
     #path('add/<int:pk>', PostAdd.as_view(), name='update'),
     path('<int:pk>/edit', PostUpdate.as_view(), name='update'),
-    path("<int:pk>/delete'", PostDelete.as_view(), name="delete"),
+    path("<int:pk>/delete", PostDelete.as_view(), name="delete"),
     #path("", PostList.as_view()),
     path('<int:pk>', PostDetail.as_view(), name="post"),
     #path("", showresults),
